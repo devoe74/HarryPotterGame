0881080f737edf1f32982fca45e65445bc535eea
daniel sasi <daniel.sasi.74@gmail.com>
2023-05-28 18:56:44 +0300

    path

--- src/game1/StartHomePage.java
+++ src/game1/StartHomePage.java
@@ -3,6 +3,7 @@ package game1;
 import javax.swing.*;
 import java.awt.*;
 
+
 public class StartHomePage extends JPanel {
 
     public static final int START_HOME_PAGE1_X = 0;
@@ -15,8 +16,7 @@ public class StartHomePage extends JPanel {
     public void paintComponent (Graphics graphics) {
         super.paintComponent(graphics);
 
-        ImageIcon startHomePageImage = new ImageIcon("C:\\Users\\daniel\\Desktop\\myGame\\good\\final\\StartBackground1.2.2.6.jpg");
+        ImageIcon startHomePageImage = new ImageIcon("C:\\Users\\daniel\\IdeaProjects\\HarryPotterGame\\resources\\images\\StartBackground1.2.2.6.jpg");
         startHomePageImage.paintIcon(null, graphics, START_HOME_PAGE1_X, START_HOME_PAGE1_Y);
     }
-
 }
