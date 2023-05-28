e4b67f1df5a05ea9f1111b287db4a23a4db9896e
daniel sasi <daniel.sasi.74@gmail.com>
2023-05-28 19:20:18 +0300

    path test

--- src/game1/Utils.java
+++ src/game1/Utils.java
@@ -1,5 +1,8 @@
 package game1;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 
 public class Utils {
     public static void sleep (int millis) {
@@ -8,6 +11,17 @@ public class Utils {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+    public static ImageIcon loadFile(String str){
+        ImageIcon imageIcon = null;
+        try {
+            imageIcon = new ImageIcon(ImageIO.read(new File(str)));
+
+        }catch (Exception e){
+            System.out.println("Error\n"+e);
+        }
+        return imageIcon;
+
     }
 
     public static boolean checkCollision (Rectangle rect1, Rectangle rect2) {
